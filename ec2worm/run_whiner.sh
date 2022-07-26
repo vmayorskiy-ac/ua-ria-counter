9b099e0864db94b3e427ba793d5b26a274634a28
vmayorskiy-ac <vmayorskiy@acrotron.com>
2022-07-26 19:29:13 -0400

    adding run_whiner.sh

---
+++ ec2worm/run_whiner.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+RIA_DIR=/home/ubuntu/git/ua-ria-counter/ec2worm
+LOG=$RIA_DIR/log.out
+
+echo "XXXXXXXXXXXXXXXXXXXXXXXXX" > $LOG
+echo `date` >> $LOG
+#cd $RIA_DIR
+python $RIA_DIR/ria_comment.py >> $LOG
+echo "XXXXXXXXXXXXXXXXXXXXXXXXX" >> $LOG
+cat $LOG
+
+
+#echo "XXXXXXXXXXXXXXXXXXXXXXXXX"
+#date
+#echo "*************************"
+#python $RIA_DIR/ria.py
+#echo "XXXXXXXXXXXXXXXXXXXXXXXXX"
+#cat $LOG
+
+
